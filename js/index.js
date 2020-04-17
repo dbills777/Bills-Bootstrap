9425a92e10b8f13e0ddc5b1502582e4299752a7a
dbills777 <dbills777@gmail.com>
2020-04-16 19:38:05 -0600

    added gsap

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // Custom JS here
 console.log('this is inside the js file')
+
+let testItem = document.querySelector('.btn-click')
+
+testItem.addEventListener('click', event =>{
+    console.log("is it workin")
+    gsap.to(".testing", {
+        duration: 1, 
+        y: 100, 
+        rotation: 360, 
+        scale: 0.5
+    })
+})
